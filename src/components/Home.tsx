f6ba54dfa3140c1af15c57b89967c8358b64fbf8
bokonist <jayrm1995@gmail.com>
2021-05-27 03:49:13 +0530

    finished home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,36 @@
 import "../styles/Home.css";
-import splash from "../assets/splash.png"
+import splash from "../assets/splash.png";
 import { Link } from "react-router-dom";
 
-interface Props{
+interface Props {}
 
-}
-
-const Home:React.FC<Props> = ()=> {
+const Home: React.FC<Props> = () => {
   return (
     <div>
-        <img src={splash} alt="background wallpaper" className="splash-image"></img>
-        <ul className="navigation">
-            <li className="nav-list-item"><Link className="nav-link" to="/shop">SHOP</Link></li>
-            <li className="nav-list-item"><Link className="nav-link" to="/shop/item/2">ABOUT</Link></li>
-            <li className="nav-list-item"><Link className="nav-link" to="/shop/item/2">CONTACT</Link></li>
-        </ul>
+      <img
+        src={splash}
+        alt="background wallpaper"
+        className="splash-image"
+      ></img>
+      <ul className="navigation">
+        <li className="nav-list-item">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link className="nav-link" to="/shop/item/2">
+            ABOUT
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link className="nav-link" to="/shop/item/2">
+            CONTACT
+          </Link>
+        </li>
+      </ul>
     </div>
   );
-}
+};
 
 export default Home;
