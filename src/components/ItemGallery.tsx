ca1da0ae803340dda6871b3b56b05e1abd6323db
bokonist <jayrm1995@gmail.com>
2021-05-27 03:50:15 +0530

    item gallery+cart logic

--- src/components/ItemGallery.tsx
+++ src/components/ItemGallery.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { InventoryContext } from "../contexts/InventoryContext";
 import "../styles/ItemGallery.css";
 import ItemCard from "./ItemCard";
 
-interface Product{
+interface Product {
   id: string;
   name: string;
   description: string;
@@ -11,43 +12,41 @@ interface Product{
   image: string;
 }
 
-interface Props{
-  items:Product[];
+interface Props {
   display: string;
 }
 interface ParamTypes {
   categoryName: string;
 }
 
-const ItemGallery:React.FC<Props> = (props)=> {
-  const {categoryName} = useParams<ParamTypes>();
-  const {items ,display} = props;
-  const [categoryItems , setCategoryItems]= useState<Product[]>([]);
+const ItemGallery: React.FC<Props> = (props) => {
+  const { categoryName } = useParams<ParamTypes>();
+  const { display } = props;
+  const [categoryItems, setCategoryItems] = useState<Product[]>([]);
   //console.log('itemgallery',display, items, categoryName);
-
-  useEffect(()=>{
-    if(display==="all")
-    {
+  const items = useContext(InventoryContext);
+  useEffect(() => {
+    if (display === "all") {
+      setCategoryItems(items);
+    } else if (display === "filter") {
+      setCategoryItems(
+        items.reduce((acc: Product[], item) => {
+          if (item.category.toLowerCase() === categoryName) acc.push(item);
+          return acc;
+        }, [])
+      );
+    }
+    if (categoryName === "all") {
       setCategoryItems(items);
     }
-    else if(display==="filter")
-    {
-      setCategoryItems(items.reduce((acc:Product[],item)=>{
-        if(item.category.toLowerCase() === categoryName)
-          acc.push(item);
-        return acc;
-      },[]))
-    }
-  },[items,categoryName,display]);
+  }, [items, categoryName, display]);
   return (
- 
-      <div className="item-card-container">
-        {categoryItems.map((item,index)=>{
-          return <ItemCard key={`card-${index}`} details={item}></ItemCard>
-        })}
-      </div>
+    <div className="item-card-container">
+      {categoryItems.map((item, index) => {
+        return <ItemCard key={`card-${index}`} details={item}></ItemCard>;
+      })}
+    </div>
   );
-    
-}
+};
 
 export default ItemGallery;
