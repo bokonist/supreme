d72484824f7e7486b5677bf5871fd0cf61a1f585
bokonist <jayrm1995@gmail.com>
2021-05-27 03:50:50 +0530

    prettier formatting

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,20 +1,20 @@
 import "../styles/Navigation.css";
-import logo from "../assets/supreme-logo.jpg"
-import cart_icon from "../assets/shopping-cart.svg"
+import logo from "../assets/supreme-logo.jpg";
+import cart_icon from "../assets/shopping-cart.svg";
 
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-interface Props{
+interface Props {}
 
-}
-
-const Navigation:React.FC<Props> = ()=> {
+const Navigation: React.FC<Props> = () => {
   return (
     <div className="header">
-      <Link to="/"><img src={logo} className="logo" alt="supreme logo"/></Link>
-      <img src={cart_icon} className="cart-icon" alt="shopping cart"/>
+      <Link to="/">
+        <img src={logo} className="logo" alt="supreme logo" />
+      </Link>
+      <img src={cart_icon} className="cart-icon" alt="shopping cart" />
     </div>
   );
-}
+};
 
 export default Navigation;
