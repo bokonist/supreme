80eaac6ed68c62b2d31e1a4013cddc641a2d83f4
bokonist <jayrm1995@gmail.com>
2021-05-27 03:53:01 +0530

    prettier formatting

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,7 +1,6 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Shop from "../components/Shop";
-import ItemDetail from "../components/ItemDetail";
 import { Error404 } from "../components/utility-components/404";
 
 export const Routes = () => {
@@ -9,7 +8,7 @@ export const Routes = () => {
     <Router>
       <Switch>
         <Route exact path="/" component={Home}></Route>
-        <Route  path="/shop" component={Shop}></Route>
+        <Route path="/shop" component={Shop}></Route>
         <Route path="/" component={Error404}></Route>
       </Switch>
     </Router>
