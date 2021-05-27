6dcb563d5a44e34d2a23dc5856fcaa7f2dc1cbb1
bokonist <jayrm1995@gmail.com>
2021-05-28 04:49:37 +0530

    cart components complete

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -9,6 +9,52 @@ const Cart: React.FC<Props> = () => {
   let cartItems = useContext(CartContext);
   return (
     <div className="cart-container">
+      <div className="cart-total-details">
+        <div className="cart-info">
+          <p>
+            <span>Total ({cartItems.length}) items</span>
+          </p>
+          <p>
+            <span>Items Total: </span>
+            <span className="cart-total-pretax">
+              $
+              {cartItems.reduce((acc, item) => {
+                acc += item.price * item.quantity;
+                return acc;
+              }, 0)}
+            </span>
+          </p>
+          <p>
+            <span>Taxes: </span>
+            <span className="cart-total-taxes">
+              $
+              {cartItems.reduce((acc, item) => {
+                acc += item.price * item.quantity * 0.05;
+                return acc;
+              }, 0)}
+            </span>
+          </p>
+          <p>
+            <span>Shipping: </span>
+            <span className="cart-total-shipping">${cartItems.length * 5}</span>
+          </p>
+          <p className="cart-total-amount-container">
+            <span>Total: </span>
+            <span className="cart-total-amount">
+              $
+              {cartItems.reduce((acc, item) => {
+                acc +=
+                  item.price * item.quantity +
+                  item.price * item.quantity * 0.05 +
+                  5;
+                return acc;
+              }, 0)}
+            </span>
+          </p>
+        </div>
+
+        <button className="checkout-button">CHECKOUT</button>
+      </div>
       {cartItems.map((item, index) => {
         return <CartItem key={`cart-${index}`} item={item} />;
       })}
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import "../styles/ItemCard.css";
+import "../styles/CartItem.css";
 import loadingGIF from "../assets/loading.gif";
 import { CartUpdaterContext } from "./App";
 
@@ -26,41 +26,67 @@ const CartItem: React.FC<Props> = (props) => {
   }, [item.image]);
   return (
     <div className="cart-item">
-      <img src={image || loadingGIF} alt={item.name}></img>
-      <p className="cart-item-price">
-        <span>${item.price}</span>
-        {item.quantity ? (
-          <span>
-            <span> x{item.quantity}</span>
-            <span className="cart-item-calculated-price">
-              {" $"}
-              {item.quantity * item.price}
+      <img
+        src={image || loadingGIF}
+        alt={item.name}
+        className="cart-item-image"
+      ></img>
+      <div className="cart-item-group1">
+        <p className="cart-item-name">{item.name}</p>
+        <p className="cart-item-price">
+          <span>${item.price}</span>
+          {item.quantity ? (
+            <span>
+              <span> x{item.quantity}</span>
+              <span className="cart-item-calculated-price">
+                {" $"}
+                {item.quantity * item.price}
+              </span>
             </span>
+          ) : null}
+        </p>
+        <div className="cart-item-actions">
+          {item.quantity > 0 ? (
+            <div className="cart-item-quantity-adjustor">
+              <button
+                className="cart-item-decrement-button"
+                onClick={() => {
+                  CartDispatch({ type: "remove", item: item });
+                }}
+              >
+                -
+              </button>
+              <p className="cart-item-quantity">{item.quantity}</p>
+              <button
+                className="cart-item-increment-button"
+                onClick={() => {
+                  CartDispatch({ type: "add", item: item });
+                }}
+              >
+                +
+              </button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+      <div className="cart-calculations">
+        <span>
+          <span>Tax(5%) : </span>
+          <span className="cart-item-taxes">
+            ${item.price * item.quantity * 0.05}
           </span>
-        ) : null}
-      </p>
-      <div className="cart-item-actions">
-        {item.quantity > 0 ? (
-          <div className="cart-item-quantity-adjustor">
-            <button
-              className="cart-item-decrement-button"
-              onClick={() => {
-                CartDispatch({ type: "remove", item: item });
-              }}
-            >
-              -
-            </button>
-            <p className="cart-item-quantity">{item.quantity}</p>
-            <button
-              className="cart-item-increment-button"
-              onClick={() => {
-                CartDispatch({ type: "add", item: item });
-              }}
-            >
-              +
-            </button>
-          </div>
-        ) : null}
+        </span>
+        <span>
+          <span>Shipping : </span>
+          <span className="cart-item-shipping">{"$5.00"}</span>
+        </span>
+        <span>
+          <span>Item total : </span>
+          <span className="cart-item-total">
+            $
+            {item.price * item.quantity + item.price * item.quantity * 0.05 + 5}
+          </span>
+        </span>
       </div>
     </div>
   );
